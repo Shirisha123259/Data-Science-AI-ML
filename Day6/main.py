50fb391c8b824449f9067b32f8183f4ca0168948
Shirisha123259 <shirishamca5@gmail.com>
2026-02-09 19:19:37 +0530

    Add files via upload

---
+++ Day6/main.py
@@ -0,0 +1,12 @@
+import math_operations
+
+# Using power function
+result_power = math_operations.power(2, 10)
+
+# Using average function
+numbers = [10, 20, 30, 40]
+result_average = math_operations.average(numbers)
+
+# Output
+print("2^10 =", result_power)
+print("Average =", result_average)
