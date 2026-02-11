63c76e0878a00cb94d2f8c63a706233a8b880eee
Shirisha123259 <shirishamca5@gmail.com>
2026-02-11 20:42:23 +0530

    Add files via upload

--- Day8/task1.py
+++ Day8/task1.py
@@ -1,1 +1,7 @@
-
+import numpy as np
+data = np.arange(24)
+reshaped_data = data.reshape(4, 3, 2)
+final_array = reshaped_data.transpose(0, 2, 1)
+print("Final Shape:", final_array.shape)
+print("\nFinal Array:")
+print(final_array)
